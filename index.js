8595fb3df9decc0f2bc1e7bbaf7c174ba4991c95
rawaha1 <rawaha.anik.1@gmail.com>
2024-03-13 00:02:56 +0600

    implemented socket io- base structure

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
-
+const { Server } = require('socket.io');
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+const socketio = new Server(expressServer);
 
 expressServer.listen(8080, () => {
   console.log('server running @ 8080');
