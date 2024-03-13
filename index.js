038a125a77fff113be5b246e2e01ba3afdc517fd
rawaha1 <rawaha.anik.1@gmail.com>
2024-03-13 12:06:37 +0600

    Namespace BroadCast

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
+
 const { Server } = require('socket.io');
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 const socketio = new Server(expressServer);
-let cnt = 0;
-socketio.on('connection', (socket) => {
-  console.log('new User Connected');
-  cnt++;
-  console.log(`number of users: ${cnt}`);
-  socketio.sockets.emit('myBroadCast', 'hello everyone !');
-  socket.on('disconnect', () => {
-    console.log('User Disconnected');
-    cnt--;
-    console.log(`number of users: ${cnt}`);
-  });
+
+const buyNsp = socketio.of('/buy');
+buyNsp.on('connection', () => {
+  buyNsp.emit('myEvent', 'Hello Buy');
 });
+
+const sellNsp = socketio.of('/sell');
+sellNsp.on('connection', () => {
+  sellNsp.emit('myEvent', 'Hello sell');
+});
+
 expressServer.listen(8080, () => {
   console.log('server running @ 8080');
 });
