9dd91f81dcbe42f5f0e9d306cc39519aa66ecb17
rawaha1 <rawaha.anik.1@gmail.com>
2024-03-13 10:08:18 +0600

    send data from client to server

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ socketio.on('connection', (socket) => {
   //   socket.send('Rawaha Anik');
   // }, 10000);
 
-  setInterval(() => {
-    let d = new Date();
-    let t = d.getTime();
-    // socket.send(t);
-    socket.emit('rawaha', `This is ${t}`);
-  }, 1000);
-  socket.on('disconnect', () => {
-    console.log('User Disconnected');
+  // setInterval(() => {
+  //   let d = new Date();
+  //   let t = d.getTime();
+  //   // socket.send(t);
+  //   socket.emit('rawaha', `This is ${t}`);
+  // }, 1000);
+  // socket.on('disconnect', () => {
+  //   console.log('User Disconnected');
+  // });
+  socket.on('message', (message) => {
+    console.log(message);
   });
 });
 expressServer.listen(8080, () => {
